639bca76f424ba44df4eb019f3afbaf2b2edb63f
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-12-02 15:53:01 +0700

    create a static void main for a main menu

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace StackUsingLinkedList_CSarp
             if (top == null)
                 return (true);
             else
-                return
+                return;
         }
         public void push(int element)
         {
@@ -42,15 +42,32 @@ namespace StackUsingLinkedList_CSarp
         {
             Node tmp;
             if (empty()) //If stack is empty
-                Console.WriteLine("\nStack Empty")
+                Console.WriteLine("\nStack Empty");
             else
             {
                 //Traverse the list from beggining till end
-                for(tmp = top; tmp != null; tmp = tmp.next)
+                for (tmp = top; tmp != null; tmp = tmp.next)
                 {
                     Console.WriteLine(tmp.info);
                 }
                 Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n+++Stack Menu+++\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your Choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
             }
         }
     }
