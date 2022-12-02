9920ba2cef0986f0693436633da385d9a5cb3c89
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-12-02 15:58:34 +0700

    create case 4 for the invalid option

--- Program.cs
+++ Program.cs
@@ -85,6 +85,11 @@ namespace StackUsingLinkedList_CSarp
                     case '3':
                         s.display();
                         break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
                 }
             }
         }
