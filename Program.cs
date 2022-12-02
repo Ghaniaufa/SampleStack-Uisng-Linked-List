94e3d1bca50f1e1a8d8bef1901e044b87076f001
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-12-02 15:40:26 +0700

    creat a class Stack with insert the Method for public stacks and bool

--- Program.cs
+++ Program.cs
@@ -12,4 +12,21 @@ namespace StackUsingLinkedList_CSarp
             next = n;
         }
     }
+    
+    class Stacks
+    {
+        Node top;
+
+        public Stacks()
+        {
+            top = null;
+        }
+        bool empty ()
+        {
+            if (top == null)
+                return (true);
+            else
+                return
+        }
+    }
 }
